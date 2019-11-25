d8d9386fe7e93c274fa05f8cd4c13a3006cd816f
einelson8i <einelson8i@gmail.com>
2019-11-25 09:45:46 -0700

    Update neural_network_test.py

--- neural_network_test.py
+++ neural_network_test.py
@@ -10,14 +10,7 @@ from sklearn.neural_network import MLPRegressor
 import pickle
 
 
-#  This might be needed to scale our pictures so they are the same size...
-'''
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-'''
+
 
 
 # import test and train data
@@ -32,29 +25,38 @@ y_train = img_to_array(train2)
 x_test = img_to_array(test1)
 y_test = img_to_array(test2)
 
+#  This might be needed to scale our pictures so they are the same size...
 '''
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
 It might be easier to convert from LAB values to Vector LAB values so that the network doesnt have to deal with a 3d array?
 Here I just decided to convert to a d2 array for now
 We could also train and run 3 networks, one for each dimension of our 3d array. a newtork for the L, A and B values seperatley
 '''
-# reshape train vectors so network can read them
 nsamples, nx, ny = x_train.shape
 x_train = x_train.reshape((nsamples,nx*ny))
 nsamples, nx, ny = y_train.shape
 y_train = y_train.reshape((nsamples,nx*ny))
 
+
 # train a basic network
 mlp = MLPRegressor(hidden_layer_sizes=(8,8,8), activation='relu', solver='adam', max_iter=500)
 mlp.fit(x_train,y_train)
 
+
 # predict passed in images
 predictions = mlp.predict(x_test)
 
-# will have to convert back to a 3d array?
+# will have to convert back to a 3d array
+
 
 # revert to rgb values
 back_to_rgb = color.lab2rgb(predictions)
 
-# # export to image
+# # export array to image
 img_pil = array_to_img(back_to_rgb)
 img_pil.show()
