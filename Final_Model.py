7aff37b2155a6d7194c752b44b03823597e06d87
Ethan Nelson <36721383+einelson8i@users.noreply.github.com>
2019-12-14 14:19:04 -0700

    Update Final_Model.py

--- Final_Model.py
+++ Final_Model.py
@@ -76,7 +76,7 @@ model.compile(optimizer='adam',loss='mse') # loss='sparse_categorical_crossentro
 # there is an issue fitting the data
 for e in tqdm(range(10000)):
     for i,j in enumerate(x):
-        model.fit(x=x[i],y=y[i], batch_size=50,verbose=1, epochs=1)
+        model.fit(x=x[i],y=y[i], batch_size=50,verbose=0, epochs=1)
 
 # evaluate model
 # model.evaluate(x, y, batch_size=1)
